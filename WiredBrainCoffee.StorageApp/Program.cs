7efa3f4e8a58854985b64a59902840bf88396db4
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-04-25 01:36:05 +0530

    create Generic Repository class

---
+++ WiredBrainCoffee.StorageApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using WiredBrainCoffee.StorageApp.Entities;
+using WiredBrainCoffee.StorageApp.Repositories;
+
+namespace WiredBrainCoffee.StorageApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employeeRepository = new GenericRepository<Employee>();
+            employeeRepository.Add(new Employee { FirstName = "John" });
+            employeeRepository.Add(new Employee { FirstName = "Carter" });
+            employeeRepository.Add(new Employee { FirstName = "Basoom" });
+            employeeRepository.Save();
+
+            var organizationRepository = new GenericRepository<Organization>();
+            organizationRepository.Add(new Organization { Name = "Apple Inc." });
+            organizationRepository.Add(new Organization { Name = "Tesla Inc." });
+            organizationRepository.Add(new Organization { Name = "Boring Company" });
+            organizationRepository.Save();
+        }
+    }
+}
---
+++ WiredBrainCoffee.StorageApp/Repositories/GenericRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WiredBrainCoffee.StorageApp.Entities;
+
+namespace WiredBrainCoffee.StorageApp.Repositories
+{
+    public class GenericRepository<T>
+    {
+        private readonly List<T> _items = new List<T>();
+
+        public void Add(T item)
+        {
+            _items.Add(item);
+        }
+
+        public void Save()
+        {
+            foreach (T item in _items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
